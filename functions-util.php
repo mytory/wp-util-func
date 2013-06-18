63f368b318cf37650ec0a62bed9a1b14ce28ba17
mytory <mytory@gmail.com>
2013-06-18 22:22:49 +0900

    썸네일 만드는 함수

--- functions-util.php
+++ functions-util.php
@@ -38,3 +38,37 @@ if( ! function_exists('fu_get_img_src')){
         return $img_src;
     }
 }
+
+if( ! function_exists('fu_get_thumb_src')){
+
+    /**
+     * 첨부 파일 이미지의 id를 받아서 원하는 사이즈로 썸네일을 만들고, src를 가져 오는 함수.
+     * @param  int $attachment_id
+     * @param  int $width
+     * @param  int $height
+     * @return string 썸네일의 URL
+     */
+    function fu_get_thumb_src($attachment_id, $width, $height){
+        $filepath = get_attached_file($attachment_id);
+        $upload_path = wp_upload_dir();
+        $basedir = $upload_path['basedir'];
+
+        // filepath가 $basedir까지 포함하고 있는 경우가 있음.
+        $filepath = str_replace($basedir, '', $filepath);
+        $fullpath = $basedir . $filepath;
+        if( ! is_file($fullpath)){
+            return FALSE;
+        }
+        $pathinfo = pathinfo($fullpath);
+        $new_fullpath = $pathinfo['dirname'] . '/' . $pathinfo['filename'] . "-{$width}x{$height}" . '.' . $pathinfo['extension'];
+        if( ! is_file($new_fullpath)){
+            $image = wp_get_image_editor($fullpath);
+            if ( ! is_wp_error( $image ) ) {
+                $image->resize( $width, $height, false );
+                $image->save( $new_fullpath );
+            }
+        }
+        $new_filepath = str_replace($basedir, '', $new_fullpath);
+        return $upload_path['baseurl'] . $new_filepath;
+    }
+}

bffbec5f35aa72e8bc80eed8a8f3156731f680fc
mytory <mytory@gmail.com>
2013-07-03 17:35:20 +0900

    fu_get_list_url, 리퍼러를 사용하지 않게 했다.

--- functions-util.php
+++ functions-util.php
@@ -76,36 +76,87 @@ if( ! function_exists('fu_get_thumb_src')){
 
 if( ! function_exists('fu_get_list_url')){
     /**
-     * post 형식 글의 single 페이지에 달 목록 버튼의 링크를 리턴한다. 일단, 리퍼러가 있으면 리퍼러를 출력한다.
-     * 리퍼러가 없는 경우엔 자동으로 taxonomy와 term을 가져와서 링크를 만든다.
+     * post 형식 글의 single 페이지에 달 목록 버튼의 링크를 리턴한다. 
+     * 1페이지에 들어가는 글의 개수는 워드프레스 읽기 설정으로 가정하고 계산한다. 
+     * 자동으로 taxonomy와 term을 가져와서 링크를 만든다.
      * 여러 개의 taxonomy와 여러 개의 term에 속한 글인 경우엔
      * 워드프레스가 돌려준 배열의 첫 번째 놈을 선택해서 링크를 돌려 준다.
-     * @param null|string $taxonomy
-     * @param null|string $term
-     * @return string|WP_Error
+     * @param null $taxonomy
+     * @param null $term
+     * @return string|void
      */
-    function fu_get_list_url($taxonomy = NULL, $term = NULL){
-        global $post;
-        if(isset($_SERVER['HTTP_REFERER'])){
-            return $_SERVER['HTTP_REFERER'];
+    function fu_get_list_url($taxonomy = NULL, $term_slug = NULL){
+        global $post, $table_prefix;
+
+        if( ! $taxonomy){
+            $taxonomies = get_post_taxonomies();
+            $taxonomy = $taxonomies[0];
+        }
+
+        if( ! $term_slug){
+            $terms = wp_get_post_terms($post->ID, $taxonomy);
+            $term = $terms[0];
         }else{
-            if( ! $taxonomy){
-                $taxonomies = get_post_taxonomies();
-                $taxonomy = $taxonomies[0];
-            }
-            if( ! $term){
-                $terms = wp_get_post_terms($post->ID, $taxonomy);
-                $term = $terms[0];
-            }
-            if($term){
+            $term = get_term_by('slug', $term_slug, $taxonomy);
+        }
 
-                // term 로드 결과 있으면
-                return get_term_link($term);
-            }else{
-
-                // 없으면(custom post type의 경우 term이 아예 없을 수 있다.)
-                return home_url('?post_type=' . $post->post_type);
+        if($term){
+            $sql = "SELECT SQL_CALC_FOUND_ROWS {$table_prefix}posts.ID
+                FROM {$table_prefix}posts
+                INNER JOIN {$table_prefix}term_relationships ON ( {$table_prefix}posts.ID = {$table_prefix}term_relationships.object_id )
+                WHERE 1 =1
+                AND (
+                {$table_prefix}term_relationships.term_taxonomy_id
+                IN ( $term->term_id )
+                )
+                AND {$table_prefix}posts.post_type = '{$post->post_type}'
+                AND (
+                {$table_prefix}posts.post_status = 'publish'
+                OR {$table_prefix}posts.post_status = 'private'
+                )
+                GROUP BY {$table_prefix}posts.ID
+                ORDER BY {$table_prefix}posts.post_date DESC";
+            $result = mysql_query($sql);
+            while($row = mysql_fetch_array($result)){
+                $ids[] = $row['ID'];
             }
+
+            $current_index = 0;
+            foreach ($ids as $index => $ID) {
+                if($ID == $post->ID){
+                    $current_index = $index + 1;
+                }
+            }
+            $curr_page = ceil($current_index / get_option('posts_per_page'));
+
+            // term 로드 결과 있으면
+            return get_term_link($term) . "/page/" . $curr_page;
+        }else{
+
+            $sql = "SELECT SQL_CALC_FOUND_ROWS {$table_prefix}posts.ID
+                FROM {$table_prefix}posts
+                WHERE 1 =1
+                AND {$table_prefix}posts.post_type = '{$post->post_type}'
+                AND (
+                {$table_prefix}posts.post_status = 'publish'
+                OR {$table_prefix}posts.post_status = 'private'
+                )
+                ORDER BY {$table_prefix}posts.post_date DESC";
+            $result = mysql_query($sql);
+            while($row = mysql_fetch_array($result)){
+                $ids[] = $row['ID'];
+            }
+
+            $current_index = 0;
+            foreach ($ids as $index => $ID) {
+                if($ID == $post->ID){
+                    $current_index = $index + 1;
+                }
+            }
+            $curr_page = ceil($current_index / get_option('posts_per_page'));
+
+            // 없으면(custom post type의 경우 term이 아예 없을 수 있다.)
+            return home_url('/page/' . $curr_page . '/?post_type=' . $post->post_type);
         }
     }
 }

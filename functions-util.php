80f695ee3623654f16c458228d905bde8f090d81
mytory <mytory@gmail.com>
2013-07-05 15:18:49 +0900

    fu_get_count_no 추가.

--- functions-util.php
+++ functions-util.php
@@ -257,3 +257,17 @@ if( ! function_exists('fu_get_post_navi')){
         return $navi;
     }
 }
+
+if( ! function_exists('fu_get_count_no')){
+
+    /**
+     * 카테고리 등을 게시판 형식으로 보여 줄 때 앞에 붙일 번호에 사용할 숫자를 가져 오는 함수다.
+     * 맨 윗쪽에 붙일 번호를 가져오므로, 루프돌면서 $count_no-- 를 해 주면 된다.
+     * @return mixed
+     */
+    function fu_get_count_no(){
+        global $wp_query;
+        $paged = ( $wp_query->query_vars['paged'] ? $wp_query->query_vars['paged'] : 1 );
+        return $wp_query->found_posts - ( $wp_query->query_vars['posts_per_page'] * ($paged - 1) );
+    }
+}
